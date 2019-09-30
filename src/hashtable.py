750bd09572cd5971f8a857466def2a0314b19ede
Sam <Jeffcoat>
2019-09-30 17:11:26 -0500

    Monday mvp finished

--- src/hashtable.py
+++ src/hashtable.py
@@ -4,21 +4,23 @@
 import bcrypt
 import hashlib
 
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.next = None
 
+
 class HashTable:
     '''
     A hash table that with `capacity` buckets
     that accepts string keys
     '''
+
     def __init__(self, capacity):
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
@@ -28,7 +30,6 @@ class HashTable:
         '''
         return hash(key)
 
-
     def _hash_djb2(self, key):
         '''
         Hash an arbitrary key using DJB2 hash
@@ -37,7 +38,6 @@ class HashTable:
         '''
         pass
 
-
     def _hash_mod(self, key):
         '''
         Take an arbitrary key and return a valid integer index
@@ -45,13 +45,9 @@ class HashTable:
         '''
         return self._hash(key) % self.capacity
 
-
     def insert(self, key, value):
         hashedkey = self._hash_mod(key)
         self.storage[hashedkey] = LinkedPair(key, value)
-        
-
-
 
     def remove(self, key):
         '''
@@ -61,12 +57,11 @@ class HashTable:
 
         Fill this in.
         '''
-        hashedkey= self._hash_mod(key)
+        hashedkey = self._hash_mod(key)
         if self.storage[hashedkey] == None:
             print('Warning')
         else:
             self.storage[hashedkey] = None
-
 
     def retrieve(self, key):
         '''
@@ -76,8 +71,10 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        hashedkey = self._hash_mod(key)
+        if self.storage[hashedkey] == None:
+            return None
+        return self.storage[hashedkey].value
 
     def resize(self):
         '''
@@ -86,8 +83,11 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        self.capacity += 2
+        new_storage = [None] * self.capacity
+        for i in [item for item in self.storage if item != None]:
+            new_storage[self._hash_mod(i.key)] = LinkedPair(i.key, i.value)
+        self.storage = new_storage
 
 
 if __name__ == "__main__":
